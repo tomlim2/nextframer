64dae8aa5bd8e02760c5004bf5ca1f602072789e
tomlim2 <tomandlim@gmail.com>
2022-10-27 23:08:04 +0900

    Feat: Update nextjs

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    urlImports: [
+        "https://framer.com/m/",
+        "https://framerusercontent.com/",
+        "https://ga.jspm.io/",
+        "https://jspm.dev/",
+    ],
+  },
+}
+
+module.exports = nextConfig
